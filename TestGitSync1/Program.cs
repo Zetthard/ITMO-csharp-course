fc178725fe84d445bf0e943090f407ca2b112a67
Semen Nikolaev <sr.nikolaev@gmail.com>
2021-04-26 21:00:11 +0300

    first upload

---
+++ TestGitSync1/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestGitSync1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var a = new[] { 1, 2, 3 };
+            
+            Console.Write("Please enter your name: ");
+            string name = Console.ReadLine();
+            Console.WriteLine("Hello, {0}!", name);
+
+        }
+    }
+}
