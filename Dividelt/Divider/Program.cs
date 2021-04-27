322d8fc33580508755e17b6e7aacfc3ffc2b0d25
Semen Nikolaev <sr.nikolaev@gmail.com>
2021-04-27 17:59:08 +0300

    Divider with exception handling

---
+++ Dividelt/Divider/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Divider
+{
+    class Divideit
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.Write("Введите первое число: ");
+                String s1 = Console.ReadLine();
+                Console.Write("Введите второе число: ");
+                String s2 = Console.ReadLine();
+                int a = int.Parse(s1);
+                int b = int.Parse(s2);
+                int k = a / b;
+                Console.WriteLine("Результат деления {0} на {1}: {2}", s1, s2, k);
+            }
+            catch(Exception e)
+            {
+                Console.WriteLine("Произошла досадная ошибка: {0}", e);
+            }
+        }
+    }
+}
