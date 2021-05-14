7fa16de0ff17a423b2c88be1f18b1553ac1a20fb
zetthard <sr.nikolaev@gmail.com>
2021-05-14 21:19:20 +0300

    E

---
+++ Lab08/STARTER/BANK/Test.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+public class Test
+{
+    public static void Main()
+    {
+        BankAccount firstAcc = new BankAccount();
+        BankAccount secondAcc = new BankAccount();
+        firstAcc.Populate(100);
+        secondAcc.Populate(100);
+
+        Console.WriteLine("Balance of {0} account {1} is {2}", firstAcc.Type(), firstAcc.Number(), firstAcc.Balance());
+        Console.WriteLine("Balance of {0} account {1} is {2}", secondAcc.Type(), secondAcc.Number(), secondAcc.Balance());
+
+        firstAcc.TransferFrom(secondAcc, 10);
+        Console.WriteLine("Balance of {0} account {1} after transfer is {2}", firstAcc.Type(), firstAcc.Number(), firstAcc.Balance());
+        Console.WriteLine("Balance of {0} account {1} after transfer is {2}", secondAcc.Type(), secondAcc.Number(), secondAcc.Balance());
+
+        Console.ReadKey();
+    }
+}
