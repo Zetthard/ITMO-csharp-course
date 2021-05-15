621d8755405b6d2602d9b72ac7d734a5f97b0d21
zetthard <sr.nikolaev@gmail.com>
2021-05-15 15:56:27 +0300

    Utils (ex 2) done

---
+++ Lab08/STARTER/UTILS/Test.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Utils
+{
+    public class Test
+    {
+        public static void Main()
+        {
+            string tempString;
+            Console.WriteLine("Please enter the string you wish to reverse: ");
+            tempString = Console.ReadLine();
+            Utils.Reverse(ref tempString);
+            Console.WriteLine(tempString);
+            Console.ReadKey();
+        }
+    }
+}
