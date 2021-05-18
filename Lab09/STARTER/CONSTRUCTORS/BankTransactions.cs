0bd4da5eb0f06f05331ba97b5df5ec0160bcadf5
zetthard <sr.nikolaev@gmail.com>
2021-05-18 10:08:49 +0300

    Constructors completed

---
+++ Lab09/STARTER/CONSTRUCTORS/BankTransactions.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+class BankTransactions
+{
+    private readonly decimal amount;
+    private readonly DateTime when;
+
+    public decimal Amount()
+    {
+        return amount;
+    }
+
+    public DateTime When()
+    {
+        return when;
+    }
+
+    public BankTransactions(decimal tranAmount)
+    {
+        amount = tranAmount;
+        when = DateTime.Now;
+    }
+}
+
+
